25db4531accd14260356166b79f799e4c9901b84
pokrevskii <PokrevskiiAD@alfastrah.ru>
2022-04-03 15:02:57 +0400

    add comment

--- src/main/java/com/example/demo/GitDemoApplication.java
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
+		// comment
 		SpringApplication.run(GitDemoApplication.class, args);
 	}
 
